ebbd24f29c34754ef448d0b5cade739d2e527e17
Alvin Kwon <khh2302@hotmail.com>
2018-01-15 19:18:21 +0900

    first commit

---
+++ app_mysql.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: _storage })
+var fs = require('fs');
+var app = express();
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'dmm',
+  database: 'o2'
+})
+conn.connect();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
+app.set('views', './views_mysql');
+app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+  res.render('upload'); 
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded : '+req.file.filename);
+});
+
+app.get('/topic/add', function(req, res){
+  var sql = 'SELECT id,title FROM topic';
+  conn.query(sql, function(err, topics, fields){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('add', {topics: topics});
+  });
+});
+app.post('/topic/add', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'INSERT INTO topic (title, description, author) VALUES ( ?, ?, ? )'
+  conn.query(sql, [title, description, author], function(err, result, fields){
+    if (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error')
+    } else {
+      console.log(result)
+      res.redirect('/topic/'+result.insertId);
+    }
+  })
+})
+
+
+app.get(['/topic', '/topic/:id'], function(req, res){
+  var sql = 'SELECT id,title FROM topic';
+  conn.query(sql, function(err, topics, fields){
+    var id = req.params.id;
+    if(id){
+      var sql="SELECT * FROM topic WHERE id=?";
+      conn.query(sql, [id], function ( err, topic, fields ){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error')
+        } else {
+          console.log(topic)
+          res.render( 'view', {topics: topics, topic: topic[0] });
+        }
+      })
+    } else {
+      res.render('view', {topics: topics});
+    }
+  });
+});
+
+
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
+})
