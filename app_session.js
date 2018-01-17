9a689737f4b5c38a060fd062bca51de2c96e3a75
Alvin Kwon <khh2302@hotmail.com>
2018-01-17 12:10:58 +0900

    session 기본 로그인단 구현

--- app_session.js
+++ app_session.js
@@ -17,21 +17,55 @@ app.get('/count', function(req,res){
   res.send('count :' + req.session.count);
 })
 
+// 넷째,
+// 로그 아웃시 세션 지우고 welcome으로 리다이렉션
+// welcome에서는 session.displayName 없으니깐
+// 로그인 버튼 화면 표시
+app.get('/auth/logout', function(req, res){
+  delete req.session.displayName;
+  res.redirect('/welcome')
+})
 
+// 셋째,
+// 로그인 성공시 req.sesssion.Display 있으니 체크 후
+// logout 가능 화면 표시
+// 아니면 로그인 버튼 화면 표시
+app.get('/welcome', function(req, res){
+  if(req.session.displayName){
+    res.send(`
+      <h1>Hello, ${req.session.displayName}</h1>
+      <a href="/auth/logout">logout</a>
+    `)
+  } else {
+    res.send(`
+      <h1>Welcome</h1>
+      <a href="/auth/login">Login</a>
+    `);
+  }
+})
+
+// 둘째,
+// req.body.username과 pwd를 받아서 DB의 user정보와 비교
+// DB의 displayName을 세션에 담아서 /welcome으로 redirection
+// 로그인 실패시 Who are you 전송
 app.post('/auth/login', function(req, res){
   var user = {
     username: 'egoing',
-    password: '111'
+    password: '111',
+    displayName: 'Egoing'
   }
   var uname = req.body.username;
   var pwd = req.body.password;
   if(uname === user.username && pwd === user.password){
+    req.session.displayName = user.displayName;
     res.redirect('/welcome');
   } else {
     res.send('Who Are you? <a href="/auth/login">login</a>');
   }
 })
 
+// 첫째,
+// 로그인 화면
 app.get('/auth/login', function(req, res){
   var output = `
   <h1>LOGIN</h1>
